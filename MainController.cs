3ad2d84f091ddb585d996041cc3f4c6d3a7f278a
yusuke-morioka <59383320+yusuke-morioka@users.noreply.github.com>
2020-07-24 11:30:50 +0900

    Add files via upload

---
+++ MainController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoneyBook.Controllers
+{
+    public class MainController : Controller
+    {   
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Change()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult Now()
+        {
+            return View();
+        }
+
+
+
+    }
+}
