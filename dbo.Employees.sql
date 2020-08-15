975849c081561566a821e787ba379328731ae52b
yusuke-morioka <59383320+yusuke-morioka@users.noreply.github.com>
2020-08-15 21:06:25 +0900

    Add files via upload

---
+++ dbo.Employees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]    INT IDENTITY(1,1)	NOT NULL,
+    [Days]  DATE	NOT NULL,
+    [Shop]  NVARCHAR (200)	NOT NULL,
+    [Thing] NVARCHAR (200)	NOT NULL,
+    [Price] INT	NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
